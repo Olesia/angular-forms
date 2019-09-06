19ac6729cae1249f466dc0bbcf2bd5cecf7ae385
Olesia <olesjalega@gmail.com>
2019-09-06 17:52:30 +0300

    first commit

---
+++ src/app/template-driven-forms/signup-form/signup-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserModel } from './../../models/user.model';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent implements OnInit {
+  countries: Array<string> = ['Ukraine', 'Armenia', 'Belarus', 'Hungary', 'Kazakhstan', 'Poland', 'Russia'];
+  user: UserModel = new UserModel();
+
+  constructor() {
+  }
+
+  onSave(signupForm: NgForm) {
+    // Form model
+    console.log(signupForm.form);
+    // Form value
+    console.log(`Saved: ${JSON.stringify(signupForm.value)}`);
+  }
+
+  ngOnInit() {
+
+  }
+
+}
+
+
