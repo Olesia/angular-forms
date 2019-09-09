c5755b6a4282be921841307739fc47a4a528fa9e
Olesia <olesjalega@gmail.com>
2019-09-09 12:11:46 +0300

    up to 15

--- src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
+++ src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { UserModel } from './../../models/user.model';
+import { CustomValidators } from './../../validators';
 
 @Component({
   selector: 'app-signup-reactive-form',
@@ -6,10 +9,129 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-reactive-form.component.css']
 })
 export class SignupReactiveFormComponent implements OnInit {
+  countries: Array<string> = ['Ukraine', 'Armenia', 'Belarus', 'Hungary', 'Kazakhstan', 'Poland', 'Russia'];
+  // data model
+  user: UserModel = new UserModel(
+    'Vitaliy',
+    'Zhyrytskyy',
+    'v.zhiritskiy@gmail.com',
+    false
 
-  constructor() { }
+  );
+  // form model
+  userForm: FormGroup;
+  placeholder = {
+    email: 'Email (required)',
+    phone: 'Phone'
+};
+
+rMin = 1;
+rMax = 5;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    // this.createForm();
+    // this.setFormValues();
+    // this.patchFormValues();
+    this.buildForm();
+
+  }
+
+  onSave() {
+    // Form model
+    console.log(this.userForm);
+    // Form value w/o disabled controls
+    console.log(`Saved: ${JSON.stringify(this.userForm.value)}`);
+    // Form value w/ disabled controls
+    console.log(`Saved: ${JSON.stringify(this.userForm.getRawValue())}`);
+  }
+
+  onSetNotification(notifyVia: string) {
+    const phoneControl = this.userForm.get('phone');
+    const emailControl = this.userForm.get('email');
+
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+      emailControl.clearValidators();
+      emailControl.clearAsyncValidators();
+      this.placeholder.email = 'Email';
+      this.placeholder.phone = 'Phone (required)';
+    } else {
+      emailControl.setValidators([
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'),
+        Validators.email
+      ]);
+      emailControl.setAsyncValidators(
+        CustomValidators.asyncEmailPromiseValidator
+);
+      phoneControl.clearValidators();
+      this.placeholder.email = 'Email (required)';
+      this.placeholder.phone = 'Phone';
+    }
+    phoneControl.updateValueAndValidity();
+    emailControl.updateValueAndValidity();
+  }
+
+  private createForm() {
+    this.userForm = new FormGroup({
+      firstName: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(3)],
+        updateOn: 'blur'
+      }),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      phone: new FormControl(),
+      notification: new FormControl('email'),
+      serviceLevel: new FormControl('', {
+        validators: [CustomValidators.serviceLevel],
+        updateOn: 'blur'
+      }),
+
+      sendProducts: new FormControl(true)
+    });
+  }
+
+  private setFormValues() {
+    this.userForm.setValue({
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      email: this.user.email,
+      sendProducts: this.user.sendProducts
+    });
+  }
+
+  private patchFormValues() {
+    this.userForm.patchValue({
+      firstName: this.user.firstName,
+      lastName: this.user.lastName
+    });
+  }
+
+  private buildForm() {
+    this.userForm = this.fb.group({
+      // firstName: ['', [Validators.required, Validators.minLength(3)]],
+      // It works!
+      firstName: new FormControl('', {validators: [Validators.required, Validators.minLength(3)], updateOn: 'blur'}),
+      // It works since v7
+      // firstName: this.fb.control('', { validators: [Validators.required, Validators.minLength(3)], updateOn: 'blur' }),
+
+      lastName: [
+        { value: 'Zhyrytskyy', disabled: false },
+        [Validators.required, Validators.maxLength(50)]
+      ],
+      email: [
+        '',
+        [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'), Validators.email],
+        // [CustomValidators.asyncEmailPromiseValidator]
+      ],
+      serviceLevel: [''],
+      sendProducts: true,
+      phone: '',
+      notification: 'email',
+
+    });
   }
 
 }
---
+++ src/app/validators/custom.validators.ts
@@ -0,0 +1,52 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+// rxjs
+import { Observable } from 'rxjs';
+
+
+export class CustomValidators {
+  static serviceLevel(c: AbstractControl): { [key: string]: boolean } | null {
+    console.log('Validator: serviceLevel is called');
+    return checkServiceLevel(c);
+  }
+
+  static serviceLevelRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): { [key: string]: boolean } | null => {
+      return checkServiceLevel(c, min, max);
+    };
+  }
+
+  static asyncEmailPromiseValidator(
+    c: AbstractControl
+  ):
+    | Promise<{ [key: string]: any } | null> | Observable<{ [key: string]: any } | null> {
+    const email = c.value;
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if (email === 'existsemail@example.com') {
+          resolve({
+            asyncEmailInvalid: true
+          });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+  }
+}
+
+export function checkServiceLevel(
+    c: AbstractControl,
+    min: number = 1,
+    max: number = 5
+  ): { [key: string]: boolean } | null {
+    if (
+      c.value !== undefined &&
+      (Number.isNaN(c.value) || c.value < min || c.value > max)
+    ) {
+      return {
+        serviceLevel: true
+      };
+    }
+    return null;
+  }
